3468b9254279f376742f1fb15c64f5fea64c96fa
XSimba123 <40066081+XSimba123@users.noreply.github.com>
2019-06-22 23:14:33 +0800

    Add files via upload

---
+++ 最终版（老师请看这个）/js/model.js
@@ -0,0 +1,15 @@
+window.model = {
+    data: {
+        items: [
+            // {msg:'', completed: false}
+        ],
+        imp: 'All',
+        msg: '',
+        filter: 'All'
+    },
+    TOKEN: 'DoOrDie'
+
+    // data provider interface
+    // init: null
+    // flush: null
+};
